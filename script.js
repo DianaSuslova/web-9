20949dba76f0d6323799318b3abacb8187b10b9a
DianaSuslova <1680793@mail.ru>
2021-01-28 15:03:17 +0300

    сделала 3 и 4 задания

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ function arrayCut(a){
     else console.log('1');
   }
 }
+
+b = String;
+function isPalindrome(b){
+  let r = 0;
+  for (let i = 0; i<=b.length-1; i++) {
+    if (b[i] == b[b.length - i-1]) r++;
+  }
+  return((r == b.length));
+}
+
+
+q = [];
+let answer = '';
+function scalarMultiplication(n, q) {
+  for (let i = 0; i<q.length;i++){
+    answer = '';
+    for (let j = 0; j<q.length; j++){
+      q[i][j]*=n;
+      answer += q[i][j] + '   ';
+    }
+    console.log(`${answer}`);
+  }
+}
